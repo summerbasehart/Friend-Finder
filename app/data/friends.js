b2aab1e1d3638a7f32e95ab4922a4d3d04caaadf
unknown <summerbasehart@gmail.com>
2019-07-02 00:02:02 -0700

    updated routes and survey

---
+++ app/data/friends.js
@@ -0,0 +1,20 @@
+var friendList = [
+    {
+      name: 'Summer Grace',
+      profilePic: "",
+      scores: [
+        "4",
+        "5",
+        "5",
+        "2",
+        "4",
+        "5",
+        "4",
+        "4",
+        "3",
+        "4"
+      ]
+    }
+  ]
+  
+  module.exports = friendList;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,4 +1,8 @@
+var path = require('path');
+
 const db = require('../server.js')
+
+var friends = require('../data/friends.js');
 
 module.exports = app => {
   // Create
