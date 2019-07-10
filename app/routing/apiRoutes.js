059727c5418336261f12b31aa9a186ff826289f5
unknown <summerbasehart@gmail.com>
2019-07-10 12:35:51 -0700

    updated survey

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,12 +1,14 @@
 var path = require('path');
 
-const db = require('../server.js')
+// const db = require('../server.js')
 
 var friends = require('../data/friends.js');
 
 module.exports = app => {
   // Create
-  
+    app.post("/api/friends", function(req, res) {
+      console.log(req.body)
+    })
   // Read
 
   // Update
--- server.js
+++ server.js
@@ -2,10 +2,20 @@ const { createConnection } = require('mysql2')
 const express = require('express')
 const path = require('path')
 
-module.exports = createConnection({
-  host: 'localhost',
-  port: '3306',
-  user: 'root',
-  password: 'Phil!1993',
-  database: 'friendFinder_db'
+// module.exports = createConnection({
+//   host: 'localhost',
+//   port: '3306',
+//   user: 'root',
+//   password: 'Phil!1993',
+//   database: 'friendFinder_db'
+// })
+
+const app = express()
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.listen(3000, function() {
+  console.log("server running")
 })
+
+require("./app/routing/apiRoutes")(app)
+require("./app/routing/htmlRoutes")(app)
